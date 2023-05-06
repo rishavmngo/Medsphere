9bf236a332f924fd70f2af86d7bbf39ccf543a16
rishavmngo <rishavinmngo@gmail.com>
2023-05-06 10:56:00 +0530

    updating users.sql

--- server/db/schemas/users.sql
+++ server/db/schemas/users.sql
@@ -1,20 +1,27 @@
--- public.prescribed_medicine definition
+-- public.users definition
 
 -- Drop table
 
--- DROP TABLE public.prescribed_medicine;
+-- DROP TABLE public.users;
 
-CREATE TABLE public.prescribed_medicine (
-	id serial4 NOT NULL,
-	prescription_id int4 NOT NULL,
-	medicine_id int4 NOT NULL,
-	dosage text NULL,
-	duration text NULL,
-	CONSTRAINT prescribed_medicine_pkey PRIMARY KEY (id)
+CREATE TABLE public.users (
+	uid serial4 NOT NULL,
+	displayname varchar(100) NOT NULL,
+	email varchar(100) NOT NULL,
+	"password" varchar(300) NOT NULL,
+	organisation_id int4 NULL,
+	is_organisation bool NULL DEFAULT false,
+	department_id int4 NULL,
+	age int4 NULL,
+	address text NULL,
+	phone_number text NULL,
+	qualifications text NULL,
+	CONSTRAINT users_email_key UNIQUE (email),
+	CONSTRAINT users_pkey PRIMARY KEY (uid)
 );
 
 
--- public.prescribed_medicine foreign keys
+-- public.users foreign keys
 
-ALTER TABLE public.prescribed_medicine ADD CONSTRAINT prescribed_medicine_medicine_id_constraint FOREIGN KEY (medicine_id) REFERENCES public.medicine(id) ON DELETE CASCADE;
-ALTER TABLE public.prescribed_medicine ADD CONSTRAINT prescribed_medicine_prescription_id_constraint FOREIGN KEY (prescription_id) REFERENCES public.prescription(id) ON DELETE CASCADE;
+ALTER TABLE public.users ADD CONSTRAINT user_department_fk_department_id FOREIGN KEY (department_id) REFERENCES public.department(id);
+ALTER TABLE public.users ADD CONSTRAINT users_organisation_id_fkey FOREIGN KEY (organisation_id) REFERENCES public.users(uid);
