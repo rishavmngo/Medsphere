d5eeeddcc86feacefa9fe72c68122bab3dccb9a0
rishavmngo <rishavinmngo@gmail.com>
2023-05-21 12:09:30 +0530

    modify prescription component

--- client/src/component/prescription/prescription.component.jsx
+++ client/src/component/prescription/prescription.component.jsx
@@ -2,7 +2,7 @@ import './prescription.style.css'
 import Group1 from '../group1/group1.component'
 import Group2 from '../group2/group2.component'
 import Group3 from '../group3/group3.component'
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useRef } from 'react'
 import { PrescriptionContext } from '../../context/prescription.context'
 import { useParams } from 'react-router-dom'
 import { parseInt } from 'lodash'
@@ -12,19 +12,22 @@ function Prescription() {
   const { currentPrescription, getPrescrcriptionById } =
     useContext(PrescriptionContext)
   const { prescriptionId } = useParams()
+  const input = useRef(0)
   useEffect(() => {
     getPrescrcriptionById(parseInt(prescriptionId))
   }, [])
 
   const handleExport = () => {
-    const input = document.getElementById('prescription')
-    printDocument(input)
+    printDocument(input.current)
   }
+  useEffect(() => {
+    console.log(input)
+  }, [input])
 
   return (
     <>
       <button onClick={handleExport}>print</button>
-      <div id='prescription' className='prescription'>
+      <div id='prescription' className='prescription' ref={input}>
         <div className='groups'>
           <Group1 data={currentPrescription} />
           <Group2 data={currentPrescription} />
